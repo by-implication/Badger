1075ba438821478f48fc60642472f110d8e21b97
Levi Tan Ong <levi@byimplication.com>
2013-11-16 13:17:17 +0800

    templatize!
    Signed-off-by: Levi Tan Ong <levi@byimplication.com>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,12 @@
 function Main($scope, $http){
 	
 	$scope.status = 'working';
-
+	$scope.children = [
+		{
+			'description': 'Daniel Fordan',
+			'amount': 20000000,
+			'rating': 3.5,
+			'numrates': 1000
+		}
+	]
 }
