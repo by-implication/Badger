786153625a426b2184ea56d6e5b1ff874466ae71
Levi Tan Ong <levi@byimplication.com>
2013-11-17 01:46:49 +0800

    Webfont! styles form inputs
    Signed-off-by: Levi Tan Ong <levi@byimplication.com>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -101,7 +101,12 @@ function App($scope, $http, $location){
 
 	$scope.$watch(function(){ return $location.absUrl(); }, function(newPath, oldPath){
 		$http.get('/meta', {params: $location.search()})
-		.success(function(r){ $scope.focus = r; });
+		.success(function(r){ 
+			$scope.focus = r; 
+			if(r.lat && r.lng){
+				map.setView([r.lat, r.lng], 6)
+			}
+		});
 	});
 
 	$scope.cats = {
