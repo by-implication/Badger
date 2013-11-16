b902af335bf41c3102a3146f50b64e8185f97a20
Levi Tan Ong <levi@byimplication.com>
2013-11-16 23:13:23 +0800

    adds directives for leaf and loc detail
    Signed-off-by: Levi Tan Ong <levi@byimplication.com>

--- app/support/Security.scala
+++ app/support/Security.scala
@@ -5,6 +5,7 @@ import play.api.mvc.Results.Redirect
 import play.api.mvc.{Action, BodyParser, Controller, Request, RequestHeader, Result}
 import scala.concurrent.duration._
 import budget.models._
+import controllers.routes
 
 trait Secured {
   self: Controller =>
@@ -28,7 +29,7 @@ object Secured {
 
   def login(user: User)(implicit request: RequestHeader) = {
     Redirect(routes.Application.app)
-    .withSession(request.session + ("user_id" -> user.id.toString)))
+    .withSession(request.session + ("user_id" -> user.id.toString))
   }
 
   def logout()(implicit user: User, request: RequestHeader) = {
