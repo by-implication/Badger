da33669d446f2a6e0d2946c21de11eb7b66ab202
Levi Tan Ong <levi@byimplication.com>
2014-01-11 17:08:52 +0800

    makes directive for map to work even with ng-if for the modal
    Signed-off-by: Levi Tan Ong <levi@byimplication.com>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -60,6 +60,23 @@ app
 				}
 			}
 		}
+	})
+	.directive('biMap', function(){
+		return {
+			link: function(scope, elm, attrs){
+				var map = L.map('map').setView([14.612209, 121.0527097], 7);
+
+				// add an OpenStreetMap tile layer
+				L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				}).addTo(map);
+
+				// add a marker in the given location, attach some popup content to it and open the popup
+				L.marker([14.612209, 121.0527097]).addTo(map)
+				    .bindPopup('omg wow such map<br> html <strong><em>works</em></strong>!<p><em>here is best company evar</em></p>')
+				    .openPopup();
+			}
+		}
 	});
 
 //////////////////////////////////////////// services ////////////////////////////////////
